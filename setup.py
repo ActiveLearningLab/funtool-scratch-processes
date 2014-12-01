0a2199923c0f4eb02451676fe839d6c6da24c52a
Philip Janisiewicz <pjanisiewicz@gmail.com>
2014-12-01 17:45:57 +0100

    added setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funtool-scratch-processes',
+        version='0.0.25',
+        description='process to be used with the FUN Tool to analyze Scratch projects',
+        author='Active Learning Lab',
+        author_email='pjanisiewicz@gmail.com',
+        license='MIT',
+        packages=['funtool_scratch_processes'],
+        install_requires=['funtool'],
+        zip_safe=False)
