e24d6250191f3a0082c1bebbaca527b938d35326
Philip Janisiewicz <pjanisiewicz@gmail.com>
2015-02-23 20:13:18 +0100

    fixed wheel dist setup

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
 
 setup(name='funtool-scratch-processes',
-        version='0.0.31',
+        version='0.0.32',
         description='process to be used with the FUN Tool to analyze Scratch projects',
         author='Active Learning Lab',
         author_email='pjanisiewicz@gmail.com',
         license='MIT',
-        packages=['funtool_scratch_processes'],
+        packages=[
+            'funtool_scratch_processes',
+            'funtool_scratch_processes.adaptors',
+            'funtool_scratch_processes.analysis_selectors',
+            'funtool_scratch_processes.group_measures',
+            'funtool_scratch_processes.grouping_selectors',
+            'funtool_scratch_processes.reporters',
+            'funtool_scratch_processes.state_measures'
+        ],
         install_requires=['funtool'],
         classifiers=[ 
             'Development Status :: 3 - Alpha',
